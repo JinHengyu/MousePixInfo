5b2a585f9b5a153fd273c214c927a9327c60710f
JinHengyu <2195868682@qq.com>
2018-06-07 17:31:04 +0800

    新计划宣布堕胎

---
+++ nodeJS实现/magnifier.js
@@ -0,0 +1,13 @@
+// global['magnifier'] = document.querySelector('magnifier')
+// global['canvas'] = document.querySelector('#magnifier canvas')
+
+// let array = robot.captureScreen(0, 0, 10, 10).image
+// let base64Data = btoa(String.fromCharCode.apply(null, array));
+
+// let image = document.createElement('img')
+
+// image.src = 'data:image/jpg;base64,' + base64Data;
+// document.body.innerHTML=''
+// document.body.appendChild(image)
+
+// // canvas.getContext('2d').drawImage(, 0, 0)
